be3907989c4beeb79f589f5ad5f563ae5bea0113
Jeff Bai <j.bai@wustl.edu>
2020-05-14 08:51:19 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,8 +4,8 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(nameArray)
-  nameArray each.do |name|
-    return "Hello, my name is #{name}."
+  i = 0
+  while i < nameArray.length
 end
 
 def assign_rooms(speakers)
