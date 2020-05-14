0e76d190fac448e03568d751c45c24db3e3c5b58
Jeff Bai <j.bai@wustl.edu>
2020-05-14 09:06:04 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,5 +11,5 @@ end
 
 def assign_rooms(speakers)
   roomArray = []
-  speakers.each_with_index {|name, i| nameArray << "Hello, #{name}! You'll be assigned to room #{i}!"
+  speakers.each_with_index {|name, i| nameArray << "Hello, #{name}! You'll be assigned to room #{i}!"}
 end
